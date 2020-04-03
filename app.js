df3f19fe3e018001ec51ff502fc8223404e7dd6f
Lauren <lauren.finley1@yahoo.com>
2020-04-03 01:47:14 -0500

    add onclick event to menu items

--- app.js
+++ app.js
@@ -6,21 +6,42 @@ var firebaseConfig = {
     storageBucket: "restaurant-menu-a59ea.appspot.com",
     messagingSenderId: "907944859697",
     appId: "1:907944859697:web:c7615254b2ee873942dd10"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  const dbRef = firebase.database().ref();
-  const menuItemsRef = dbRef.child('menuItems');
 
-  const menuItemsListUI = document.getElementById('menu-item-list');
+const dbRef = firebase.database().ref();
+const menuItemsRef = dbRef.child('menuItems');
 
-  menuItemsRef.on("child_added", snap => {
+const menuItemsListUI = document.getElementById('menu-item-list');
+
+function updateSelectedElement(item) {
+    document.getElementById("menu-item-name").innerHTML = item.name;
+    document.getElementById("menu-item-price").innerHTML = "$" + item.price;
+    document.getElementById("menu-item-ingredients").innerHTML = item.ingredients;
+}
+
+function menuItemClicked(e) {
+    let itemID = e.target.getAttribute("child-key");
+    const itemRef = dbRef.child('menuItems/' + itemID);
+    const item = {};
+
+    itemRef.on("child_added", snap => {
+        item[snap.key] = snap.val();
+    });
+
+    updateSelectedElement(item);
+}
+
+menuItemsRef.on("child_added", snap => {
     let menuItem = snap.val();
     let listItem = document.createElement("li");
     listItem.innerHTML = menuItem.name;
     listItem.setAttribute("child-key", snap.key);
-    //$li.addEventListener("click", menuItemClicked); 
+    listItem.addEventListener("click", menuItemClicked); 
     menuItemsListUI.append(listItem);
-  });
+    
+    //set default selected item
+    if (snap.key == 0) { updateSelectedElement(menuItem)}
+});
